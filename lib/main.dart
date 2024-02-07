63785e99eca6715ac3dbed398042d36fecf8f79b
Ammad Naseer <sp20-bcs-156@cuiwah.edu.pk>
2024-02-07 18:20:33 +0500

    onboarding + login

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
+import 'package:loginapp/views/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:loginapp/views/onboarding.dart';
 
 void main() {
@@ -15,7 +19,64 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Login Demo',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      home: const OnboardingScreen(),
+      home: const AppStart(),
+    );
+  }
+}
+
+class AppStart extends StatefulWidget {
+  const AppStart({Key? key}) : super(key: key);
+
+  @override
+  _AppStartState createState() => _AppStartState();
+}
+
+class _AppStartState extends State<AppStart> {
+  late Future<bool> _isFirstTime;
+
+  @override
+  void initState() {
+    super.initState();
+    _isFirstTime = checkFirstTime();
+  }
+
+  Future<bool> checkFirstTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
+    return isFirstTime;
+  }
+
+  Future<void> setNotFirstTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isFirstTime', false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: _isFirstTime,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text('Error: ${snapshot.error}'),
+            ),
+          );
+        } else {
+          bool isFirstTime = snapshot.data!;
+          if (isFirstTime) {
+            return const OnboardingScreen();
+          } else {
+            return const LoginPage();
+          }
+        }
+      },
     );
   }
 }
